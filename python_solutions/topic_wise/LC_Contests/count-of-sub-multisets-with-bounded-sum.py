8b0d548b8d20addda6dbd229659dc5c48112b50e
rahulyad011 <rahulzyadav87@gmail.com>
2023-10-15 12:43:01 -0400

    adding readme for day 6-7 and LC contest

---
+++ python_solutions/topic_wise/LC_Contests/count-of-sub-multisets-with-bounded-sum.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/contest/biweekly-contest-115/problems/count-of-sub-multisets-with-bounded-sum/
+
+class Solution:
+    def countSubMultisets(self, nums: List[int], l: int, r: int) -> int:
+        
