8506bd533227b5cdc2c17513de006ba1678ce2e0
Rahul Yadav <121466610+rahulyad011@users.noreply.github.com>
2023-11-20 19:36:13 -0500

    Update cutting_rod.py

--- python_solutions/topic_wise/DP/cutting_rod.py
+++ python_solutions/topic_wise/DP/cutting_rod.py
@@ -1,76 +1,74 @@
+#User function Template for python3
+
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        int_max = 100000
-        """Recursive solution"""
-        # def coinChangeUtil(coins, n , amount):
-        #     if amount==0:
+    def cutRod(self, price, n):
+        #code here
+        """recursive solution"""
+        # def cutRodUtils(price, left, ind):
+        #     if ind==0 or left==0:
         #         return 0
-        #     if n==0:
-        #         if amount!=0:
-        #             return int_max
-        #     if coins[n-1]>amount:
-        #         return coinChangeUtil(coins, n-1, amount)
+        #     if ind <= left:
+        #         return max(cutRodUtils(price, left, ind-1) , price[ind-1]+cutRodUtils(price, left-ind, ind))
         #     else:
-        #         return min(coinChangeUtil(coins, n-1, amount), 1 + coinChangeUtil(coins, n, amount-coins[n-1]))
-        # n = len(coins)
-        # result = coinChangeUtil(coins, n, amount)
-        # if result == int_max:
-        #     return -1
-        # else:
-        #     return result
-
-        """memorization with dp - all testcases passing"""
-        # dp = None
-        # def coinChangeUtil(coins, n , amount):
-        #     if amount==0:
-        #         dp[n][amount] = 0
-        #         return dp[n][amount]
-        #     if n==0:
-        #         if amount!=0:
-        #             dp[n][amount]=int_max
-        #             return dp[n][amount]
-        #     if dp[n][amount]!=-1:
-        #         return dp[n][amount]
-        #     if coins[n-1]>amount:
-        #         dp[n][amount] = coinChangeUtil(coins, n-1, amount)
-        #         return dp[n][amount]
+        #         return cutRodUtils(price, left, ind-1)
+        # return cutRodUtils(price, n, n)
+        
+        """memorization solution - passes 1110/1115"""
+        # length = n
+        # dp = [[-1 for i in range(length+1)] for j in range(n+1)]
+        # def cutRodUtils(price, ind, left):
+        #     if ind==0 or left ==0 :
+        #         dp[ind][left] = 0
+        #         return dp[ind][left]
+        #     if dp[ind][left] != -1:
+        #         return dp[ind][left]
+        #     if ind <= left:
+        #         dp[ind][left] = max(cutRodUtils(price, ind-1, left ) , price[ind-1]+cutRodUtils(price, ind, left-ind))
+        #         return dp[ind][left]
         #     else:
-        #         dp[n][amount] = min(coinChangeUtil(coins, n-1, amount), 1 + coinChangeUtil(coins, n, amount-coins[n-1]))
-        #         return dp[n][amount]
-        # n = len(coins)
-        # dp = [[-1 for i in range(amount+1)] for j in range(n+1)]
-        # result = coinChangeUtil(coins, n, amount)
-        # if result == int_max:
-        #     return -1
-        # else:
-        #     return result
-
-        """Top down DP approach 2D dp -- passes all test cases"""
-        n = len(coins)
-        dp = [[0 for i in range(amount+1)] for i in range(n+1)]
-
-        for i in range(amount+1):
-            # first row i.e n=0
-            dp[0][i] = int_max
-
-        for i in range(n+1):
-            #first column i.e amount=0
-            dp[i][0] = 0 
+        #         dp[ind][left]= cutRodUtils(price, ind-1, left)
+        #         return dp[ind][left]
+        # return cutRodUtils(price, n, length)
         
-        for i in range(1, n+1):
-            for j in range(1, amount+1):
-                if coins[i-1] <= j:
-                    dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coins[i-1]])
-                else:
-                    dp[i][j] = dp[i-1][j]
-        if dp[n][amount]!=int_max:
-            return dp[n][amount]
-        else:
-            return -1
-
-        """Top down DP approach 1D dp -- fastest"""
-
-        # for revision work
-
-
+        """top down solution with 2D dp - passes 1113/1115"""
+        # # length = range(1, n+1)
+        # dp = [[0 for i in range(n+1)] for j in range(n+1)]
+        # # dp[i][j] where j represents the length of the rode and 
+        # # i represents the current selected length(element)
+        # for i in range(1, n+1):
+        #     for j in range(1, n+1):
+        #         if i<= j:
+        #             dp[i][j] = max(dp[i-1][j], price[i-1] + dp[i][j-i ])
+        #         else:
+        #             dp[i][j] = dp[i-1][j]
+        # return dp[n][n] 
         
+        """top down solution with 1D dp - passes all"""
+        dp = [0] * (n + 1)
+    
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if i <= j:
+                    dp[j] = max(dp[j], price[i - 1] + dp[j - i])
+        return dp[n]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.cutRod(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
